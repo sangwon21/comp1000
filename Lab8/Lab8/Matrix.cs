8d5e4d5cd6e5c3e006be30383cec6b5e4cfa1f67
sangwon <junothyme@gmail.com>
2022-11-11 18:43:23 +0900

    feat: DotProduct 메서드 작성

--- Lab8/Lab8/Matrix.cs
+++ Lab8/Lab8/Matrix.cs
@@ -1,8 +1,19 @@
 ﻿using System;
 namespace Lab8
 {
-    public struct Matrix
+    public static class Matrix
     {
+        public static int DotProduct(int[] v1, int[] v2)
+        {
+            int sum = 0;
+
+            for(int i = 0; i < v1.Length; ++i)
+            {
+                sum += v1[i] * v2[i];
+            }
+
+            return sum;
+        }
     }
 }
 
