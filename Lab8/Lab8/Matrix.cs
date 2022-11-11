44f34f56f1cfb7bd8451e7d90d4e0a131a946d7b
sangwon <junothyme@gmail.com>
2022-11-11 22:30:21 +0900

    feat: Transpose 메서드 작성

--- Lab8/Lab8/Matrix.cs
+++ Lab8/Lab8/Matrix.cs
@@ -1,5 +1,4 @@
-﻿using System;
-namespace Lab8
+﻿namespace Lab8
 {
     public static class Matrix
     {
@@ -7,12 +6,27 @@ namespace Lab8
         {
             int sum = 0;
 
-            for(int i = 0; i < v1.Length; ++i)
+            for (int i = 0; i < v1.Length; ++i)
             {
                 sum += v1[i] * v2[i];
             }
 
             return sum;
+        }
+
+        public static int[,] Transpose(int[,] matrix)
+        {
+            int[,] outMatrix = new int[matrix.GetLength(1), matrix.GetLength(0)];
+
+            for (int i = 0; i < matrix.GetLength(0); ++i)
+            {
+                for (int j = 0; j < matrix.GetLength(1); ++j)
+                {
+                    outMatrix[j, i] = matrix[i, j];
+                }
+            }
+
+            return outMatrix;
         }
     }
 }
