d2e0309351d8f36ffcc5e6ddf8c445a1b92fa3e4
sangwon <junothyme@gmail.com>
2022-11-28 21:28:17 +0900

    feat: GetFrequencyTable 메서드 작성

---
+++ Lab11/Lab11/FrequencyTable.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab11
+{
+    public static class FrequencyTable
+    {
+        public static List<Tuple<Tuple<int, int>, int>> GetFrequencyTable(int[] data, int maxBinCount)
+        {
+            int maxValue = Int32.MinValue;
+            int minValue = Int32.MaxValue;
+
+            for (int i = 0; i < data.Length; ++i)
+            {
+                maxValue = Math.Max(data[i], maxValue);
+                minValue = Math.Min(data[i], minValue);
+            }
+
+            int diff = maxValue - minValue;
+            int minInterval = diff / maxBinCount;
+
+            if (minInterval < 0)
+            {
+                return null;
+            }
+
+            if (minInterval == 0)
+            {
+                List<Tuple<Tuple<int, int>, int>> outList = new List<Tuple<Tuple<int, int>, int>>(1);
+
+                Tuple<int, int> period = new Tuple<int, int>(maxValue, maxValue + 1);
+                outList.Add(new Tuple<Tuple<int, int>, int>(period, data.Length));
+
+                return outList;
+            }
+
+            for (int i = minInterval; i < diff; ++i)
+            {
+                List<Tuple<Tuple<int, int>, int>> outList = tryPutDataIntoHistogram(data, minValue, maxValue, i, maxBinCount);
+
+                if (outList != null)
+                {
+                    return outList;
+                }
+            }
+
+            return null;
+        }
+
+        private static List<Tuple<Tuple<int, int>, int>> tryPutDataIntoHistogram(int[] data, int start, int end, int interval, int maxBinCount)
+        {
+            List<Tuple<Tuple<int, int>, int>> list = new List<Tuple<Tuple<int, int>, int>>(maxBinCount);
+
+            bool isValid = true;
+
+            for (int i = start; i < end; i += interval)
+            {
+                Tuple<int, int> period = new Tuple<int, int>(i, i + interval);
+                list.Add(new Tuple<Tuple<int, int>, int>(period, 0));
+            }
+
+            foreach (int datum in data)
+            {
+                int index = (datum - start) / interval;
+
+                if (index >= maxBinCount)
+                {
+                    isValid = false;
+                    break;
+                }
+
+                Tuple<int, int> currentPeriod = list[index].Item1;
+                int currentCount = list[index].Item2;
+
+                list[index] = new Tuple<Tuple<int, int>, int>(currentPeriod, currentCount + 1);
+            }
+
+            if (isValid == false)
+            {
+                return null;
+            }
+
+            return list;
+        }
+    }
+}
--- Lab11/Lab11/Program.cs
+++ Lab11/Lab11/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace Lab11
 {
@@ -6,7 +8,116 @@ namespace Lab11
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] data = new int[] { 4 };
+            List<Tuple<Tuple<int, int>, int>> frequencyTable = FrequencyTable.GetFrequencyTable(data, 1);
+
+            Debug.Assert(getTotalCount(frequencyTable) == data.Length);
+
+            Debug.Assert(frequencyTable.Count == 1);
+            Debug.Assert(frequencyTable[0].Item1.Item1 == 4);
+            Debug.Assert(frequencyTable[0].Item1.Item2 == 5);
+            Debug.Assert(frequencyTable[0].Item2 == 1);
+
+            frequencyTable = FrequencyTable.GetFrequencyTable(data, 5);
+
+            Debug.Assert(getTotalCount(frequencyTable) == data.Length);
+
+            Debug.Assert(frequencyTable.Count == 1);
+            Debug.Assert(frequencyTable[0].Item1.Item1 == 4);
+            Debug.Assert(frequencyTable[0].Item1.Item2 == 5);
+            Debug.Assert(frequencyTable[0].Item2 == 1);
+
+            data = new int[] { 4, 4, 4, 4 };
+            frequencyTable = FrequencyTable.GetFrequencyTable(data, 3);
+
+            Debug.Assert(getTotalCount(frequencyTable) == data.Length);
+
+            Debug.Assert(frequencyTable.Count == 1);
+            Debug.Assert(frequencyTable[0].Item1.Item1 == 4);
+            Debug.Assert(frequencyTable[0].Item1.Item2 == 5);
+            Debug.Assert(frequencyTable[0].Item2 == 4);
+
+            data = new int[] { 4, 5, 6, 7, 8, 9 };
+            frequencyTable = FrequencyTable.GetFrequencyTable(data, 5);
+
+            Debug.Assert(getTotalCount(frequencyTable) == data.Length);
+            Debug.Assert(frequencyTable.Count == 3);
+
+            Debug.Assert(frequencyTable[0].Item1.Item1 == 4);
+            Debug.Assert(frequencyTable[0].Item1.Item2 == 6);
+            Debug.Assert(frequencyTable[0].Item2 == 2);
+
+            Debug.Assert(frequencyTable[1].Item1.Item1 == 6);
+            Debug.Assert(frequencyTable[1].Item1.Item2 == 8);
+            Debug.Assert(frequencyTable[1].Item2 == 2);
+
+            Debug.Assert(frequencyTable[2].Item1.Item1 == 8);
+            Debug.Assert(frequencyTable[2].Item1.Item2 == 10);
+            Debug.Assert(frequencyTable[2].Item2 == 2);
+
+            data = new int[] { 1, 12, 14, 15, 21, 22, 22, 30, 33, 41, 41, 61, 69, 70, 81, 90, 92, 101 };
+            frequencyTable = FrequencyTable.GetFrequencyTable(data, 5);
+
+            Debug.Assert(getTotalCount(frequencyTable) == data.Length);
+            Debug.Assert(frequencyTable.Count == 5);
+
+            Debug.Assert(frequencyTable[0].Item1.Item1 == 1);
+            Debug.Assert(frequencyTable[0].Item1.Item2 == 22);
+            Debug.Assert(frequencyTable[0].Item2 == 5);
+
+            Debug.Assert(frequencyTable[1].Item1.Item1 == 22);
+            Debug.Assert(frequencyTable[1].Item1.Item2 == 43);
+            Debug.Assert(frequencyTable[1].Item2 == 6);
+
+            Debug.Assert(frequencyTable[2].Item1.Item1 == 43);
+            Debug.Assert(frequencyTable[2].Item1.Item2 == 64);
+            Debug.Assert(frequencyTable[2].Item2 == 1);
+
+            Debug.Assert(frequencyTable[3].Item1.Item1 == 64);
+            Debug.Assert(frequencyTable[3].Item1.Item2 == 85);
+            Debug.Assert(frequencyTable[3].Item2 == 3);
+
+            Debug.Assert(frequencyTable[4].Item1.Item1 == 85);
+            Debug.Assert(frequencyTable[4].Item1.Item2 == 106);
+            Debug.Assert(frequencyTable[4].Item2 == 3);
+
+            data = new int[] { 10, 11, 20, 1, 30, 20, 22, 25, 48, 43, 23, 44, 49, 21, 31, 35, 33, 36, 2, 4, 3 };
+            frequencyTable = FrequencyTable.GetFrequencyTable(data, 5);
+
+            Debug.Assert(getTotalCount(frequencyTable) == data.Length);
+            Debug.Assert(frequencyTable.Count == 5);
+
+            Debug.Assert(frequencyTable[0].Item1.Item1 == 1);
+            Debug.Assert(frequencyTable[0].Item1.Item2 == 11);
+            Debug.Assert(frequencyTable[0].Item2 == 5);
+
+            Debug.Assert(frequencyTable[1].Item1.Item1 == 11);
+            Debug.Assert(frequencyTable[1].Item1.Item2 == 21);
+            Debug.Assert(frequencyTable[1].Item2 == 3);
+
+            Debug.Assert(frequencyTable[2].Item1.Item1 == 21);
+            Debug.Assert(frequencyTable[2].Item1.Item2 == 31);
+            Debug.Assert(frequencyTable[2].Item2 == 5);
+
+            Debug.Assert(frequencyTable[3].Item1.Item1 == 31);
+            Debug.Assert(frequencyTable[3].Item1.Item2 == 41);
+            Debug.Assert(frequencyTable[3].Item2 == 4);
+
+            Debug.Assert(frequencyTable[4].Item1.Item1 == 41);
+            Debug.Assert(frequencyTable[4].Item1.Item2 == 51);
+            Debug.Assert(frequencyTable[4].Item2 == 4);
+        }
+
+        private static int getTotalCount(List<Tuple<Tuple<int, int>, int>> frequencyTable)
+        {
+            int count = 0;
+
+            foreach (var tup in frequencyTable)
+            {
+                count += tup.Item2;
+            }
+
+            return count;
         }
     }
 }
